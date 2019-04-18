a3d88fa53fb4e4145bb31ebdeee646a9837986d5
Chris Hart <chart@concord.org>
2019-04-18 16:26:51 -0400

    Switch the order of the two meal simulations and allow either one to be used on first run.

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -80,30 +80,30 @@ export class AppComponent extends BaseComponent<IProps, IState> {
             <div className="section simulation">
               <DanceSimulation dance={runningCarb ? "carb" : runningProtein ? "protein" : ""} />
               <div className="simulation-controls">
-                <div className={carbButtonClass} onClick={this.runCarbSimulation}>
-                  <div className="simulation-button-text">Carbohydrate-rich Meal</div>
-                  <div className="simulation-button-icon carb" />
-                  </div>
                 <div className={proteinButtonClass} onClick={this.runProteinSimulation}>
                   <div className="simulation-button-text">Protein-rich Meal</div>
                   <div className="simulation-button-icon protein" />
+                </div>
+                <div className={carbButtonClass} onClick={this.runCarbSimulation}>
+                  <div className="simulation-button-text">Carbohydrate-rich Meal</div>
+                  <div className="simulation-button-icon carb" />
                 </div>
               </div>
               <Attribution />
             </div>
             <div className="section chart-table">
               <div className="subsection diagram">
-                <div className="energy-diagram carb">
-                  <div>Carbohydrate-rich Meal</div>
-                  <EnergyDiagram energyInput={inputEnergyCarb} currentEnergy={currentEnergyCarb}
-                    running={runningCarb} display={runningCarb || hasRunCarbSimulation}
-                    currentHunger={currentHungerCarb} finalEnergyUsePercent={END_CARB_ENERGY_PERCENT} />
-                </div>
                 <div className="energy-diagram protein">
                   <div>Protein-rich Meal</div>
                   <EnergyDiagram energyInput={inputEnergyProtein} currentEnergy={currentEnergyProtein}
                     running={runningProtein} display={runningProtein || hasRunProteinSimulation}
                     currentHunger={currentHungerProtein} finalEnergyUsePercent={END_PROTEIN_ENERGY_PERCENT} />
+                </div>
+                <div className="energy-diagram carb">
+                  <div>Carbohydrate-rich Meal</div>
+                  <EnergyDiagram energyInput={inputEnergyCarb} currentEnergy={currentEnergyCarb}
+                    running={runningCarb} display={runningCarb || hasRunCarbSimulation}
+                    currentHunger={currentHungerCarb} finalEnergyUsePercent={END_CARB_ENERGY_PERCENT} />
                 </div>
               </div>
               <div className="energy-diagram-key">
@@ -156,7 +156,7 @@ export class AppComponent extends BaseComponent<IProps, IState> {
     const energyFunc = (x: number) => (Math.sin(x));
     const hungerFunc = (x: number) => (1 - Math.cos(4 * x / 5));
     const timeMax = Math.PI / 2;
-    if (!runningCarb && !runningProtein && hasRunCarbSimulation) {
+    if (!runningCarb && !runningProtein) {
       this.setState({
         runningProtein: true,
         inputEnergyProtein: START_PROTEIN_ENERGY,
